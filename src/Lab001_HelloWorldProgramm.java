efc15b97432cdce2ec119a02081f8b8001f2ea41
USER <sikharamsowjanya605@gmail.com>
2025-03-15 20:17:51 +0530

    15 march pushing code lab001

--- src/Lab001_HelloWorldProgramm.java
+++ src/Lab001_HelloWorldProgramm.java
@@ -1,5 +1,5 @@
 public class Lab001_HelloWorldProgramm {
     public static void main(String[] args) {
-        System.out.println("sowjanya");
+        System.out.println("sowjanya test");
     }
 }
---
+++ src/ex_01_Java_Basics/ex_02_Java.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basics;
+
+public class ex_02_Java {
+    public static void main(String[] args) {
+        System.out.println("test 2");
+    }
+}
