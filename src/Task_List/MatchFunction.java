2462298eefd6cc21c3936ad795885b1d808a9f87
USER <sikharamsowjanya605@gmail.com>
2025-03-16 12:53:26 +0530

    16 Math march pushing code lab001

--- src/Task_List/MatchFunction.java
+++ src/Task_List/MatchFunction.java
@@ -5,8 +5,8 @@ public class MatchFunction {
         int x=20;
         int y=10;
         int z=10;
-        double result = (Math.cbrt((Math.pow(x,2)+Math.pow(y,2)-Math.abs(z))));
-        System.out.println(result);
+        double results = (Math.cbrt((Math.pow(x,2)+Math.pow(y,2)-Math.abs(z))));
+        System.out.println(results);
     }
 }
 
