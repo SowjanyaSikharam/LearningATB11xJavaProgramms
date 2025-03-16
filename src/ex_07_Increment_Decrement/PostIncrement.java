d47321126e21969c94dceac3314a06d7fe8db0c1
USER <sikharamsowjanya605@gmail.com>
2025-03-16 11:42:15 +0530

    16 increment  march pushing code lab001

---
+++ src/ex_07_Increment_Decrement/PostIncrement.java
@@ -0,0 +1,9 @@
+package ex_07_Increment_Decrement;
+
+public class PostIncrement {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a++ + a++);
+        System.out.println(a);
+    }
+}
