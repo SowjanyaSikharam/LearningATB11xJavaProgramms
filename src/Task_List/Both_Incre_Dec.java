978e9951e5180cc3493f6b4b060b500b54a7c8d6
USER <sikharamsowjanya605@gmail.com>
2025-03-16 14:14:56 +0530

    16 Both_Incre_Dec march pushing code

---
+++ src/Task_List/Both_Incre_Dec.java
@@ -0,0 +1,11 @@
+package Task_List;
+
+public class Both_Incre_Dec {
+    public static void main(String[] args) {
+                int a = 20;
+                System.out.println(--a + a++ + a--);//19 + 19 + 20 = 58
+        System.out.println(a);//19
+            }
+        }
+
+
