2f9f5e986b7c9b850d4eb4b0b43346ea9b1ac894
USER <sikharamsowjanya605@gmail.com>
2025-03-19 21:49:11 +0530

    19 Forloop Programs march pushing code

---
+++ src/Loops/FroLppos.java
@@ -0,0 +1,11 @@
+package Loops;
+
+public class FroLppos
+{
+    public static void main(String[] args) {
+        for(int _2=0;_2<=10;_2++){
+            System.out.println(_2);
+
+        }
+    }
+}
